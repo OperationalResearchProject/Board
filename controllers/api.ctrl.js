5a2ee6586340beba4212e7ea9bf1acafb21856b9
Geoffrey PRUVOST <gpruvost@adexos.fr>
2017-04-19 13:53:33 +0200

    front : add top stats

---
+++ controllers/api.ctrl.js
@@ -0,0 +1,10 @@
+
+module.exports.getTransactionNumber = function () {
+
+    return 42;
+};
+
+module.exports.getUserNumber = function () {
+
+    return 2;
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var apiCtrl = require('../controllers/api.ctrl');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { transactionNumber: apiCtrl.getTransactionNumber(), userNumber: apiCtrl.getUserNumber() });
 });
 
 module.exports = router;
