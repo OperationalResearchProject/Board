06c505cec16b8870158b6c08fbec8407283e1c57
Geoffrey <geoffrey.pruvost@hotmail.com>
2017-05-12 21:45:40 +0200

    clean widgets

--- public/data/morris-data.js
+++ public/data/morris-data.js
@@ -3,115 +3,48 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
-        }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
-        }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
-        }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
-        }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
-        }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
-        }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
-        }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
-        }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
-        }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
-        }],
-        xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
-        pointSize: 2,
-        hideHover: 'auto',
-        resize: true
+            number: '1',
+            fitness: 500,
+            solution: '1-2-3-4'
+        },{
+            number: '2',
+            fitness: 150,
+            solution: '1-2-3-4'
+        },{
+            number: '3',
+            fitness: 100,
+            solution: '1-2-3-4'
+        },{
+            number: '3',
+            fitness: 70,
+            solution: '1-2-3-4'
+        },{
+            number: '4',
+            fitness: 50,
+            solution: '1-2-3-4'
+        },{
+            number: '5',
+            fitness: 40,
+            solution: '1-2-3-4'
+        },{
+            number: '6',
+            fitness: 35,
+            solution: '1-2-3-4'
+        },{
+            number: '7',
+            fitness: 33,
+            solution: '1-2-3-4'
+        },{
+            number: '8',
+            fitness: 29,
+            solution: '1-2-3-4'
+        }
+
+        ],
+        xkey: 'number',
+        ykeys: ['fitness', 'solution'],
+        labels: ['fitness', 'solution'],
+        parseTime: false
     });
 
-    Morris.Donut({
-        element: 'morris-donut-chart',
-        data: [{
-            label: "Download Sales",
-            value: 12
-        }, {
-            label: "In-Store Sales",
-            value: 30
-        }, {
-            label: "Mail-Order Sales",
-            value: 20
-        }],
-        resize: true
-    });
-
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            y: '2006',
-            a: 100,
-            b: 90
-        }, {
-            y: '2007',
-            a: 75,
-            b: 65
-        }, {
-            y: '2008',
-            a: 50,
-            b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2010',
-            a: 50,
-            b: 40
-        }, {
-            y: '2011',
-            a: 75,
-            b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
-        }],
-        xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
-        hideHover: 'auto',
-        resize: true
-    });
-    
 });
